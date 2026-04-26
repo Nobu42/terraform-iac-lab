9211db669bd48fd9aeef150ba48dba93410bfbce
Nobu42 <nobu4071@icloud.com>
2026-04-26 16:11:20 +0900

    Update vpc-setup.md!!

--- learning_aws/test.sh
+++
@@ -1,4 +0,0 @@
-aws ec2 describe-subnets \
-    --filters Name=vpc-id,Values=$VPC_ID \
-    --query 'Subnets[*].{Name:Tags[?Key==`Name`].Value | [0], AZ:AvailabilityZone, CIDR:CidrBlock, ID:SubnetId}' \
-    --output table
