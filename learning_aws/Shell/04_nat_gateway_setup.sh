dc0df38ab61999a76c9e83cf818dd7a0eac5bd8c
Nobu42 <nobu4071@icloud.com>
2026-04-29 10:31:43 +0900

    Update vpc-setup.md!!

--- learning_aws/Shell/04_nat_gateway_setup.sh
+++ learning_aws/Shell/04_nat_gateway_setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# 0. ID取得
+# ID取得
 PUB01_ID=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=sample-subnet-public01 --query 'Subnets[0].SubnetId' --output text)
 PUB02_ID=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=sample-subnet-public02 --query 'Subnets[0].SubnetId' --output text)
+
+# -------------------------------------------------------------------
+# NATゲートウェイ用の固定パブリックIP (Elastic IP) の確保
+# -------------------------------------------------------------------
+# NATゲートウェイには固定された外向きのIPが必要なため、
+# AWSのプールから一つ「割り当てID (Allocation ID)」を取得して変数に格納する。
+# ※LocalStack上では料金は発生しないが、本番環境では未使用時に課金されるので注意。
+# 構文: aws ec2 allocate-address --domain vpc --query 'AllocationId' --output text
 
 # --- NAT Gateway 01 ---
 ALLOC_ID_01=$(aws ec2 allocate-address --domain vpc --query 'AllocationId' --output text)
@@ -23,7 +31,7 @@ NGW02_ID=$(aws ec2 create-nat-gateway \
     --query 'NatGateway.NatGatewayId' --output text)
 
 echo "Waiting for NAT Gateways to become available..."
-# 本番環境ではこれがないと次のルート設定で失敗することがあります
+# 本番環境ではこれがないと次のルート設定で失敗することがある
 aws ec2 wait nat-gateway-available --nat-gateway-ids $NGW01_ID $NGW02_ID
 
 echo "NAT Gateways are READY: $NGW01_ID, $NGW02_ID"
