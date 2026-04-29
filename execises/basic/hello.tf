1dbd4851eec071540ae032cdb0d5a1b1a0a15c2c
Nobu42 <nobu4071@icloud.com>
2026-04-29 10:40:48 +0900

    Update vpc-setup.md!!

--- execises/basic/hello.tf
+++
@@ -1,6 +0,0 @@
-# hello.tf
-resource "terraform_data" "sample" {
-  provisioner "local-exec" {
-    command = "echo \"Hello, Terraform.\""
-  }
-}
--- execises/localstack_test/lambda_sources/localstack_greet/main.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    greet = os.environ['GREET']
-    print(greet, ', LocalStack!')
