fb913e5702040ae8ef44b681e66bc4a6b0837f18
Nobu42 <nobu4071@icloud.com>
2026-05-02 15:09:58 +0900

    Update 2026-05-02

--- old/learning_aws/Shell/ec2_status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "--- EC2 Instances Status ---"
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{ID:InstanceId,Type:InstanceType,IP:PublicIpAddress,Status:State.Name,Name:Tags[?Key==`Name`].Value | [0]}' --output table
-
-echo "--- Running Containers (LocalStack) ---"
-docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Ports}}"
